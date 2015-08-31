34c42e4e9d1a4071cc1a7403518e1e7af188a57b
Ever Ibarra Almaral <everibarra07@gmail.com>
2015-08-31 12:43:24 -0500

    Create playingwithintegers.cpp

---
+++ playingwithintegers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int x;
+int y;
+
+cout<<"Hello, I´m a calculator of two values"<<endl;
+cout<<""<<endl;
+cout<<" give me the first value"<<endl;
+cout<< ""<<endl;
+cin>>x;
+
+cout<<" give me the second value"<<endl;
+cout<<""<<endl;
+cin>>y;
+
+int z=y-x;
+cout<<"The addition equals to  ";
+cout<<z<<endl;
+
+int c=y+x;
+cout<<"The substraction equals to  ";
+cout<<c<<endl;
+
+int d=y*x;
+cout<<"The multiplication equals to ";
+cout<<d<<endl;
+
+int a=y/x;
+cout<<"The division is equals to  ";
+cout<<a<<endl;
+
+int b=y%x;
+cout<<"The res equals to ";
+cout<<b<<endl;
+
+
+return 0;
+
+}
